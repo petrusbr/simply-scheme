0a8319caf248adcd9587b848b1e91e2c3ee3eae1
Pedro Teixeira <pedrao@gmail.com>
2018-10-26 15:57:27 -0300

    Bridge-val implemented. End

--- projects/bridge.scm
+++ projects/bridge.scm
@@ -29,6 +29,9 @@
 (define (hand-dist-points hand)
   (accumulate + (every suit-dist-points (suit-counts hand))))
 
+(define (bridge-val hand)
+  (+ (high-card-points hand) (hand-dist-points hand)))
+
 (card-val 'cq)
 (card-val 's7)
 (card-val 'ha)
@@ -50,3 +53,6 @@
 
 (hand-dist-points '(sa s10 s7 s6 s2 hq hj h9 ck c4 dk d9 d3))
 (hand-dist-points '(h3 d7 sk s3 c10 dq d8 s9 s4 d10 c7 d4 s2))
+
+(bridge-val '(sa s10 s7 s6 s2 hq hj h9 ck c4 dk d9 d3))
+(bridge-val '(h3 d7 sk s3 c10 dq d8 s9 s4 d10 c7 d4 s2))
