529ef5524b35e8c987845630cd2d2dd1086dc453
Pedro Teixeira <pedrao@gmail.com>
2018-10-26 13:37:21 -0300

    High-card-points implemented

--- projects/bridge.scm
+++ projects/bridge.scm
@@ -11,6 +11,12 @@
       ((equal? val 'j) 1)
       (else 0))))
 
+(define (high-card-points hand)
+  (accumulate + (every card-val hand)))
+
 (card-val 'cq)
 (card-val 's7)
 (card-val 'ha)
+
+(high-card-points '(sa s10 hq ck c4))
+(high-card-points '(sa s10 s7 s6 s2 hq hj h9 ck c4 dk d9 d3))
