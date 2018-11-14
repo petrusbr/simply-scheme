9b0859a869ca30fb17aaf336e4c01c3e642e98f7
Pedro Teixeira <pedrao@gmail.com>
2018-11-13 23:07:51 -0200

    Finding Triples

--- projects/Tic-Tac-Toe/ttt.scm
+++ projects/Tic-Tac-Toe/ttt.scm
@@ -14,20 +14,21 @@
 (define WIN_COMB '(123 456 789 147 258 369 159 357))
 
 (define POS '_xo_x_o__)
+(define POS2 'x_____oxo)
 
 ; expected (1xo 4x6 14o xx8 o69 1x9 oxo) for WIN_COMB
 (define (find-triples position) 
-  (every substitute-triple WIN_COMB)
+  (every (lambda (triple) (substitute-triple triple position)) WIN_COMB)
   )
 
 ; expected xx8 for 258
-(define (substitute-triple triple)
-  (accumulate word (every substitute-letter triple))
+(define (substitute-triple triple position)
+  (accumulate word (every (lambda (l) (substitute-letter l position)) triple))
   )
 
 ; expected x for 2
-(define (substitute-letter l)
-  (let ((letter (item l POS)))
+(define (substitute-letter square position)
+  (let ((letter (item square position)))
     (if (equal? letter '_)
-      l
+      square
       letter)))
