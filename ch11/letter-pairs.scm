3f22b71617cee98a57b18fca2127964514e05838
Pedro Teixeira <pedrao@gmail.com>
2018-11-14 22:16:35 -0200

    Trying to understand the recursion

--- ch11/letter-pairs.scm
+++ ch11/letter-pairs.scm
@@ -12,19 +12,26 @@
   (se wd))
 
 (define (lp2 wd)
-  (se (word (first wd) (bf wd))))
+  (se (word (first wd) (first (bf wd)))))
 
-(define (lp3- wd)
+(define (lp3.0 wd)
   (se (bl wd) (bf wd)))
 
-(define (lp3 wd)
+(define (lp3.1 wd)
   (se (word (first wd) (first (bf wd)))
       (word (first (bf wd)) (first (bf (bf wd))))))
 
-(define (lp4 wd)
+(define (lp3.2 wd)
+  (se (lp2 wd)
+      (lp2 (bf wd))))
+
+(define (lp4.0 wd)
   (se (word (first wd) (first (bf wd)))
       (word (first (bf wd)) (first (bf (bf wd))))
       (word (first (bf (bf wd))) (first (bf (bf (bf wd)))))))
+
+(define (lp4.1 wd)
+  (se (word (first wd) (first (bf wd))) (lp3.2 (bf wd))))
 
 (define (letter-pairs wd)
   (if (or (= 2 (count wd)) (= 1 (count wd)))
